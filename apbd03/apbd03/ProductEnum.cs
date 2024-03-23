5ec84b9cb9fbfe3e37725e48f3e0e4e389a4a4e1
s27498 <s27498@pjwstk.edu.pl>
2024-03-23 18:45:11 +0100

    all classes implemented

---
+++ apbd03/apbd03/ProductEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd03;
+
+
+public enum ProductEnum
+{
+    Bananas = 13,
+    Chocolate = 18,
+    Fish = 2,
+    Meat = -15,
+    Ice_Cream = -18,
+    Frozen_pizza = -30,
+    Cheese = 7,
+    Sausages = 5,
+    Butter = 20,
+    Eggs =  19
+    
+}
